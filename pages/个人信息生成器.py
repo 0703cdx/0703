af04f8a447da8db6dad7f9eadb085208c7cd8729
0703cdx <2593448287@qq.com>
2025-10-27 16:28:13 +0800

    Add files via upload

---
+++ pages/个人信息生成器.py
@@ -0,0 +1,103 @@
+import streamlit as st
+
+
+def my_format_func(option):
+    return f'选{option}'
+
+st.header('个人信息生成器')
+a1, a2= st.columns([1,2])
+
+with a1:
+    st.subheader('个人信息表单')
+    name = st.text_input('姓名', autocomplete='这是一个占位字符串')
+
+    aa = st.text_input('职位', placeholder='这是一个占位字符串',key="1")
+
+    bb = st.text_input('电话', placeholder='这是一个占位字符串',key='2')
+
+    dd = st.text_input('邮箱', placeholder='这是一个占位字符串')
+
+    date = st.date_input("生日", value=None)
+    st.write("你选择的日期是:", date)
+ 
+    st.text('性别')
+    lunch = st.radio(
+    '你的性别是什么',
+    ['男', '女', '其他'],
+    horizontal=True,
+    label_visibility='hidden'
+    )
+
+    
+    city = st.selectbox('学历：', ['初中', '高中', '职高', '本科', '大专', '硕士', '博士'],  index=6)
+
+    
+    options_1 = st.multiselect(
+    '语言能力(可多选）',
+    ['中文', '英语', '法语', '俄语', '日语', '阿拉伯语'],
+    ['中文'],
+    max_selections=6)
+
+
+    zz= st.multiselect(
+    '技能(可多选)',
+    ['SQL', 'Python', 'Java', 'JavaScript', 'HTML\CSS', '数据分析', '机械学习', '深度学习'],
+    ['Python', 'Java'],
+    format_func=my_format_func,
+    )
+
+    my_range = range(1, 21)
+
+    numbers = st.select_slider('工作经验', options=my_range, value=5)
+    
+    values = st.slider(
+    '期望薪资',
+    5000.0, 50000.0, (10000.0, 20000.0))
+
+    
+    init_text = ""
+    st.text_area(label='个人简历:', value=init_text,
+            height=200, max_chars=20000)
+    from datetime import datetime, time
+    w = st.time_input("最佳联系时间", time(8, 45))
+    uploaded_file = st.file_uploader('图片', type=['jpg', 'jpeg', 'png', ])
+    
+
+with a2:
+    st.subheader('实时预览')
+    st.write("图片:", uploaded_file)
+    if uploaded_file is not None:
+        st.image(uploaded_file, width=150, caption="图片")
+        
+    c1, c2, c3= st.columns([1,1,1])
+    with c1:
+        st.write("职位:", aa)
+        st.write("电话:", bb)
+        st.write("邮箱:", bb)
+        st.write("生日:", date)
+        st.write("最佳联系时间:", w)
+    with c2:
+        st.write("姓名:", name)
+        st.write("性别:", lunch)
+        st.write("学历:", city)
+        st.write("工作经验:", numbers)
+        
+    with c3:
+       
+        s=''
+        for i in options_1:
+            s =s + i +","
+
+        st.write("语言能力:", s)
+        
+        v=''
+        for r in zz:
+            v =v + r +","
+        st.write("技能:", v)
+        st.write("期望薪资:", values)
+    st.write("个人简历:", init_text)  
+  
+    
+    
+    
+    
---
+++ pages/图片集.py
@@ -0,0 +1,29 @@
+import streamlit as st
+
+st.set_page_config(page_title='动物园', page_icon='🐒')
+
+# 图片数组
+images = [{'url': 'https://hakaimagazine.com/wp-content/uploads/header-proboscis-monkeys.jpg','属性':'成龙'},{
+          'url': 'https://www.pennlive.com/resizer/2s9yGlOJZ434P62jhjJw9ngtS8w=/1280x0/smart/advancelocal-adapter-image-uploads.s3.amazonaws.com/image.pennlive.com/home/penn-media/width2048/img/nation-world/photo/monkeyjpg-f0a30582aef21f0b.jpg','属性':'笑'},
+          {'url': 'https://wallpapers.com/images/hd/cute-monkey-photo-in-the-shrub-0j8nj9zrmgtu7g4o.jpg','属性':'思索'}]
+
+if 'ind' not in st.session_state:
+    st.session_state['ind'] = 0
+
+def nextImg():
+    st.session_state['ind'] = ( st.session_state['ind'] + 1) % len(images)
+
+def nextIng():
+    st.session_state['ind'] = ( st.session_state['ind'] - 1) % len(images)
+    
+st.image(images[st.session_state['ind']]['url'],caption=images[st.session_state['ind']]['属性'])
+
+
+a1,a2 = st.columns(2)
+
+with a1:
+    st.button('下一张', on_click=nextImg)
+with a2:
+    st.button('上一张', on_click=nextIng)
+
+
